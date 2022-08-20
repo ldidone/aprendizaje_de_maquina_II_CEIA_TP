1f6433bbdc41575994085fa0b42b3fa9b1f7ee70
ldidone <lucas_crar14@hotmail.com>
2022-08-20 20:42:06 -0300

    Added necessary files to create table and insert data into SQL database.

---
+++ DB/create_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE public.acoustic_extinguisher_fire_dataset (
+	"SIZE" int4 NULL,
+	"FUEL" varchar(50) NULL,
+	"DISTANCE" int4 NULL,
+	"DESIBEL" int4 NULL,
+	"AIRFLOW" float4 NULL,
+	"FREQUENCY" int4 NULL,
+	"STATUS" int4 NULL
+);
